130e3d530d324339419187c74ccec9faa4e9c701
SlickYeet <lasse@lx2.dev>
2026-08-12 18:48:16 -0700

    add context to orpc server

--- src/lib/api/server.ts
+++ src/lib/api/server.ts
@@ -3,12 +3,15 @@ import "server-only"
 import { createRouterClient } from "@orpc/server"
 import { headers } from "next/headers"
 
+import { createORPCContext } from "@/server/api"
 import { router } from "@/server/api/routers"
 
 globalThis.$api = createRouterClient(router, {
-  context: async () => ({
-    headers: await headers(),
-  }),
+  context: async () => {
+    return createORPCContext({
+      headers: await headers(),
+    })
+  },
 })
 
 export const api = globalThis.$api
