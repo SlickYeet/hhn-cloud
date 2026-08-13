998f28ecc682517e033d7e1b1e03742cc93e7936
SlickYeet <lasse@lx2.dev>
2026-08-12 17:57:55 -0700

    update package.json scripts

---
+++ src/scripts/reset-db.ts
@@ -0,0 +1,30 @@
+import { drizzle } from "drizzle-orm/postgres-js"
+import { reset } from "drizzle-seed"
+
+import { env } from "@/env"
+// import { getRedisClient } from "@/lib/redis"
+import * as schema from "@/server/db/schema"
+
+async function main() {
+  const db = drizzle(env.DATABASE_URL)
+  // const redis = getRedisClient()
+
+  try {
+    console.log("Resetting database...")
+    await reset(db, schema)
+    // await redis.flushAll()
+  } catch (error) {
+    console.error("Failed to reset database:", error)
+    process.exit(1)
+  }
+}
+
+main()
+  .then(() => {
+    console.log("Database reset successfully.")
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error("Failed to reset database:", error)
+    process.exit(1)
+  })
