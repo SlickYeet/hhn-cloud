4cbd6a9e65b39e2f3a23a01dc051942d72f7240d
SlickYeet <lasse@lx2.dev>
2026-08-12 21:47:34 -0700

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { api } from "@/lib/api/client"
 
 export default async function Page() {
-  const instance = await api.instance.getAll()
+  const instance = await api.instance.list({
+    organizationId: "org1",
+  })
 
   return (
     <main className="p-6">
       <h1 className="font-bold text-2xl">Home Page</h1>
       {instance.map((i) => (
         <div className="mt-4 border border-neutral-600 p-4" key={i.id}>
-          <p>{i.name}</p>
-          <p>{i.ipAddress}</p>
+          <p>{i.hostname}</p>
+          <p>CPU: {i.cpu}</p>
+          <p>Memory: {i.memory}</p>
+          <p>Disk: {i.disk}</p>
         </div>
       ))}
     </main>
