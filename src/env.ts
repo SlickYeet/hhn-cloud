33768dd57973a8773ee81e787039e29a600ee3c1
SlickYeet <lasse@lx2.dev>
2026-08-13 21:12:56 -0700

    add proxmox env variables

--- src/env.ts
+++ src/env.ts
@@ -11,6 +11,11 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NODE_ENV: process.env.NODE_ENV,
+    PROXMOX_HOST: process.env.PROXMOX_HOST,
+    PROXMOX_NODE: process.env.PROXMOX_NODE,
+    PROXMOX_POOL: process.env.PROXMOX_POOL,
+    PROXMOX_TOKEN_ID: process.env.PROXMOX_TOKEN_ID,
+    PROXMOX_TOKEN_SECRET: process.env.PROXMOX_TOKEN_SECRET,
     REDIS_URL: process.env.REDIS_URL,
   },
   server: {
@@ -19,6 +24,11 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    PROXMOX_HOST: z.string(),
+    PROXMOX_NODE: z.string(),
+    PROXMOX_POOL: z.string(),
+    PROXMOX_TOKEN_ID: z.string(),
+    PROXMOX_TOKEN_SECRET: z.string(),
     REDIS_URL: z.url(),
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
