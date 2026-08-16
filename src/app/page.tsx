27c6671d92db69d8e7bc67c0d0868bd1f305c36a
SlickYeet <lasse@lx2.dev>
2026-08-16 03:33:45 -0700

    log info on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { getCloudNetwork } from "@/helpers/get-cloud-network"
 import { api } from "@/server/api/client"
 
 export default async function Page() {
+  const cloudNetwork = await getCloudNetwork()
+
   const instance = await api.instance.list({
     organizationId: "org1",
   })
@@ -11,11 +14,13 @@ export default async function Page() {
       {instance.map((i) => (
         <div className="mt-4 border border-neutral-600 p-4" key={i.id}>
           <p>{i.hostname}</p>
-          <p>CPU: {i.cpu}</p>
+          <p>CPU: {i.cores}</p>
           <p>Memory: {i.memory}</p>
           <p>Disk: {i.disk}</p>
         </div>
       ))}
+
+      <p className="mt-4">Cloud Network: {JSON.stringify(cloudNetwork)}</p>
     </main>
   )
 }
