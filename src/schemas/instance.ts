e7e9e1f84db015f1a1e4453a655adff15b9e261d
SlickYeet <lasse@lx2.dev>
2026-08-16 03:02:39 -0700

    update create instance schema

--- src/schemas/instance.ts
+++ src/schemas/instance.ts
@@ -1,9 +1,12 @@
 import { insertInstanceSchema } from "@/server/db/schema"
 
 export const createInstanceSchema = insertInstanceSchema.omit({
+  cores: true,
   createdAt: true,
   deletedAt: true,
+  disk: true,
   id: true,
+  memory: true,
   networkId: true,
   pveNode: true,
   pveVmid: true,
