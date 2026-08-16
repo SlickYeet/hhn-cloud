475c897705d55d7579e23206ba555fb62de63b91
SlickYeet <lasse@lx2.dev>
2026-08-16 03:34:59 -0700

    extend create instance schema with sshKeyId

--- src/schemas/instance.ts
+++ src/schemas/instance.ts
@@ -1,16 +1,22 @@
+import * as z from "zod"
+
 import { insertInstanceSchema } from "@/server/db/schema"
 
-export const createInstanceSchema = insertInstanceSchema.omit({
-  cores: true,
-  createdAt: true,
-  deletedAt: true,
-  disk: true,
-  id: true,
-  memory: true,
-  networkId: true,
-  pveNode: true,
-  pveVmid: true,
-  rootPassword: true,
-  status: true,
-  updatedAt: true,
-})
+export const createInstanceSchema = insertInstanceSchema
+  .omit({
+    cores: true,
+    createdAt: true,
+    deletedAt: true,
+    disk: true,
+    id: true,
+    memory: true,
+    networkId: true,
+    pveNode: true,
+    pveVmid: true,
+    rootPassword: true,
+    status: true,
+    updatedAt: true,
+  })
+  .extend({
+    sshKeyId: z.string(),
+  })
