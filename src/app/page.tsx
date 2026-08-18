2083119ded031f9f03cb4758cd204e547e90eda4
SlickYeet <lasse@lx2.dev>
2026-08-18 01:07:43 -0700

    update instance info on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,40 @@
-import { getCloudNetwork } from "@/helpers/get-cloud-network"
 import { api } from "@/server/api/client"
 
 export default async function Page() {
-  const cloudNetwork = await getCloudNetwork()
-
   const instance = await api.instance.list({
     organizationId: "org1",
   })
+
+  console.log("instance", instance)
 
   return (
     <main className="p-6">
       <h1 className="font-bold text-2xl">Home Page</h1>
       {instance.map((i) => (
-        <div className="mt-4 border border-neutral-600 p-4" key={i.id}>
+        <div
+          className="mt-4 grid grid-cols-1 gap-2 border border-neutral-600 p-4 md:grid-cols-3"
+          key={i.id}
+        >
           <p>{i.hostname}</p>
           <p>CPU: {i.cores}</p>
           <p>Memory: {i.memory}</p>
           <p>Disk: {i.disk}</p>
+          <p>Status: {i.status}</p>
+          <p>Template: {i.templateId}</p>
+          <p>Network: {i.networkId}</p>
+          <p>VMID: {i.pveVmid}</p>
+          <p>Created At: {i.createdAt?.toISOString()}</p>
+          <p>Updated At: {i.updatedAt?.toISOString()}</p>
+          <p>Deleted At: {i.deletedAt?.toISOString() || "Not deleted"}</p>
+          <p>Organization ID: {i.organizationId}</p>
+          <p>IP Address: {i.ipAllocations[0]?.ipAddress}</p>
+          <p>MAC Address: {i.ipAllocations[0]?.macAddress}</p>
+          <p>Gateway: {i.ipAllocations[0]?.gateway}</p>
+          <p>VMID: {i.pveVmid}</p>
+          <p>Root Password: {i.rootPassword}</p>
+          <p>Status: {i.status}</p>
         </div>
       ))}
-
-      <p className="mt-4">Cloud Network: {JSON.stringify(cloudNetwork)}</p>
     </main>
   )
 }
