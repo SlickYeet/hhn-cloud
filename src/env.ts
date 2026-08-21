3e8bf5917405ebddd6e8764dd80614e7c3ddfde2
SlickYeet <lasse@lx2.dev>
2026-08-21 01:44:40 -0700

    add ENCRYPTION_KEY_V1

--- src/env.ts
+++ src/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
   runtimeEnv: {
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    ENCRYPTION_KEY_V1: process.env.ENCRYPTION_KEY_V1,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NODE_ENV: process.env.NODE_ENV,
     OPNSENSE_API_ENDPOINT: process.env.OPNSENSE_API_ENDPOINT,
@@ -28,6 +29,7 @@ export const env = createEnv({
   server: {
     BETTER_AUTH_SECRET: z.string(),
     DATABASE_URL: z.url(),
+    ENCRYPTION_KEY_V1: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
