ace6723c31f7d8b896d061433c39ab3c96b33695
SlickYeet <lasse@lx2.dev>
2026-08-21 00:04:29 -0700

    include delete instance worker in instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,5 +2,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./lib/api/server")
     await import("./server/workers/provision-worker")
+    await import("./server/workers/delete-instance-worker")
   }
 }
