2990a340fbdc87e59eff3399315c76b9d66f203a
SlickYeet <lasse@lx2.dev>
2026-08-24 23:30:01 -0700

    fix script error in theme provider

--- src/components/providers/theme.tsx
+++ src/components/providers/theme.tsx
@@ -4,8 +4,18 @@ import { ThemeProvider as NextThemeProvider } from "next-themes"
 import type * as React from "react"
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const scriptProps =
+    typeof window === "undefined"
+      ? undefined
+      : ({ type: "application/json" } as const)
+
   return (
-    <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      scriptProps={scriptProps}
+    >
       {children}
     </NextThemeProvider>
   )
