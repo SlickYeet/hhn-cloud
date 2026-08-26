be7f522a4d930e70467efa963ba748489302f7a0
SlickYeet <lasse@lx2.dev>
2026-08-26 02:43:53 -0700

    sonner rich colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
         <ORPCProvider>
           <ThemeProvider>
             {children}
-            <Toaster />
+            <Toaster richColors />
           </ThemeProvider>
         </ORPCProvider>
       </body>
