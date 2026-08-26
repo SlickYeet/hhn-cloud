c6f8d233cf400311403449ba62de158627334ff6
SlickYeet <lasse@lx2.dev>
2026-08-26 03:24:21 -0700

    add get icon functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,20 @@
+import {
+  IconBrandDebian,
+  IconBrandUbuntu,
+  IconBrandWindows,
+  IconSquare,
+  IconStack,
+  IconStack2,
+  IconStack2Filled,
+  IconStack3,
+  IconStack3Filled,
+  IconStackFilled,
+} from "@tabler/icons-react"
 import type { ClassValue } from "clsx"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import type { ResourcePlan } from "@/constants/resource-plans"
 import { env } from "@/env"
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,4 +25,42 @@ export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
   return env.NEXT_PUBLIC_URL
+}
+
+export function getResourcePlanIcon(planId: ResourcePlan["id"]) {
+  switch (planId) {
+    case "micro":
+      return IconStack
+    case "small":
+      return IconStackFilled
+    case "medium":
+      return IconStack2
+    case "large":
+      return IconStack2Filled
+    case "xlarge":
+      return IconStack3
+    case "2xlarge":
+      return IconStack3Filled
+    default:
+      return IconStack
+  }
+}
+
+export function getOperatingSystemIcon(os: string) {
+  switch (os.toLowerCase()) {
+    case "ubuntu":
+      return IconBrandUbuntu
+    case "debian":
+      return IconBrandDebian
+    case "centos":
+      // TODO
+      return IconSquare
+    case "fedora":
+      // TODO
+      return IconSquare
+    case "windows":
+      return IconBrandWindows
+    default:
+      return IconSquare
+  }
 }
