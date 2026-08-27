c80f222e966487d8c26be85984cf72010e1148b9
SlickYeet <lasse@lx2.dev>
2026-08-26 22:40:02 -0700

    simply reset-db.ts

--- src/scripts/reset-db.ts
+++ src/scripts/reset-db.ts
@@ -7,14 +7,9 @@ import * as schema from "@/server/db/schema"
 async function main() {
   const redis = getRedisClient()
 
-  try {
-    console.info("Resetting database...")
-    await reset(db, schema)
-    await redis.flushAll()
-  } catch (error) {
-    console.error("Failed to reset database:", error)
-    process.exit(1)
-  }
+  console.info("Resetting database...")
+  await reset(db, schema)
+  await redis.flushAll()
 }
 
 main()
