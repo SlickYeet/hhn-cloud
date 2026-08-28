c3b7198604a18cf0ccca185c9201b811364f8e58
SlickYeet <lasse@lx2.dev>
2026-08-27 17:52:04 -0700

    add dashboard link to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { IconArrowRight } from "@tabler/icons-react"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+
 export default async function Page() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
       <h1>Page</h1>
+      <Button className="mt-4" render={<Link href="/dashboard" />} size="lg">
+        Get Started <IconArrowRight />
+      </Button>
     </main>
   )
 }
