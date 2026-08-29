d3a403bb94e7d8e240cb1eaa8dd773d19aaff5fa
SlickYeet <lasse@lx2.dev>
2026-08-28 21:39:33 -0700

    add nativeButton to dashboard link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,12 @@ export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
       <h1>Page</h1>
-      <Button className="mt-4" render={<Link href="/dashboard" />} size="lg">
+      <Button
+        className="mt-4"
+        nativeButton={false}
+        render={<Link href="/dashboard" />}
+        size="lg"
+      >
         Get Started <IconArrowRight />
       </Button>
     </main>
