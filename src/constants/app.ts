df20116bc8a4c0dc43205837c112d4f29feedc12
SlickYeet <lasse@lx2.dev>
2026-09-01 00:18:47 -0700

    add cloud locations constant

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -3,3 +3,14 @@ export const APP_DESCRIPTION =
   "A PVE Cloud Compute Platform | Create and manage virtual compute on demand"
 
 export const DEFAULT_PAGE_SIZE = 10
+
+export const CLOUD_LOCATIONS = [
+  {
+    id: 1,
+    lat: 49.294,
+    lng: -122.8059,
+    location: "Coquitlam, Canada",
+    name: "Primary Private Cloud",
+    region: "ca-west-1",
+  },
+]
