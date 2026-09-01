5b3d709d2226851d0d3d54f2225a8c0af6ccef6d
SlickYeet <lasse@lx2.dev>
2026-09-01 03:14:26 -0700

    add dashboard info cards constant

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,5 @@
+import { getApiVersion, getBaseUrl } from "@/lib/utils"
+
 export const APP_NAME = "HHN Cloud"
 export const APP_DESCRIPTION =
   "A PVE Cloud Compute Platform | Create and manage virtual compute on demand"
@@ -12,5 +14,26 @@ export const CLOUD_LOCATIONS = [
     location: "Coquitlam, Canada",
     name: "Primary Private Cloud",
     region: "ca-west-1",
+  },
+]
+
+export const DASHBOARD_INFO_CARDS = [
+  {
+    description:
+      "Find all our services and features in one place. Enjoy services from media streaming to cloud computing, and everything in between, all in one convenient location.",
+    link: "https://hub.famlam.ca",
+    title: "HHN Hub",
+  },
+  {
+    description:
+      "Discover our extensive library of tutorials and guides. Learn how to create and manage virtual machines, configure networking, and optimize your cloud infrastructure.",
+    link: "https://wiki.famlam.ca",
+    title: "Tutorials",
+  },
+  {
+    description:
+      "Explore our comprehensive REST API documentation. Access detailed documentation, review API versioning, and discover all the features available to virtualize your infrastructure.",
+    link: `${getBaseUrl()}/api/v${getApiVersion()}`,
+    title: "API Docs",
   },
 ]
