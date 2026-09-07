8d9ab1cc240a119264606581d68b736b1b82a7f1
SlickYeet <lasse@lx2.dev>
2026-09-07 00:24:14 -0700

    increase instance info header height

--- src/modules/dashboard/ui/instance-details/info.tsx
+++ src/modules/dashboard/ui/instance-details/info.tsx
@@ -290,9 +290,9 @@ export function InstanceDetailsInfo({ instanceId }: { instanceId: string }) {
   if (!instance) return notFound()
 
   return (
-    <div className="mb-4 flex h-auto flex-col gap-4 md:h-16 md:flex-row md:items-center md:justify-between md:gap-0">
-      <div className="flex flex-col items-start gap-1">
-        <div className="ml-0.5 flex items-center gap-4">
+    <div className="mb-4 flex h-auto flex-col gap-4 md:h-20 md:flex-row md:items-center md:justify-between md:gap-0">
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex items-center gap-4">
           <Hint label={`Instance: ${instance.status}`} side="bottom">
             <div
               className={cn(
