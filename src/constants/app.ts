cb2df9cf96703f4d1a1392f39d13dd93bd7517eb
SlickYeet <lasse@lx2.dev>
2026-09-07 18:21:29 -0700

    increase default page size to 50

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -4,7 +4,7 @@ export const APP_NAME = "HHN Cloud"
 export const APP_DESCRIPTION =
   "A PVE Cloud Compute Platform | Create and manage virtual compute on demand"
 
-export const DEFAULT_PAGE_SIZE = 10
+export const DEFAULT_PAGE_SIZE = 50
 
 export const CLOUD_LOCATIONS = [
   {
