4782e3716c93e653f0c2c4705542e84c8c6bdaa9
SlickYeet <lasse@lx2.dev>
2026-09-07 18:21:52 -0700

    add ts type for ActivityReferenceTypeEnum

--- src/schemas/activity.ts
+++ src/schemas/activity.ts
@@ -1,7 +1,10 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod"
 import type * as z from "zod"
 
-import type { activityTypeEnum } from "@/server/db/schema"
+import type {
+  activityReferenceTypeEnum,
+  activityTypeEnum,
+} from "@/server/db/schema"
 import { activityTable } from "@/server/db/schema"
 
 export const insertActivitySchema = createInsertSchema(activityTable)
@@ -9,3 +12,5 @@ export const selectActivitySchema = createSelectSchema(activityTable)
 
 export type Activity = z.infer<typeof selectActivitySchema>
 export type ActivityTypeEnum = (typeof activityTypeEnum.enumValues)[number]
+export type ActivityReferenceTypeEnum =
+  (typeof activityReferenceTypeEnum.enumValues)[number]
