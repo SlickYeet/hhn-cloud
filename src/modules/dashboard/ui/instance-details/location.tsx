d2a3907e465ff84390a227180ce0c0fe13c74d11
SlickYeet <lasse@lx2.dev>
2026-09-10 18:13:54 -0700

    add instance options and location cards

--- src/modules/dashboard/ui/instance-details/location.tsx
+++ src/modules/dashboard/ui/instance-details/location.tsx
@@ -1,7 +1,63 @@
+"use client"
+
+import { IconMapPin } from "@tabler/icons-react"
+
+import { Hint } from "@/components/hint"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+} from "@/components/ui/item"
+import { CLOUD_LOCATIONS } from "@/constants/app"
+import { api } from "@/lib/api/client"
+import { CountryMap } from "@/modules/dashboard/ui/country-map"
+
 export function InstanceLocation({ instanceId }: { instanceId: string }) {
+  const { data: instance } = api.instance.get.useQuery({ id: instanceId })
+
+  const LOCATION_DETAILS = [
+    { description: CLOUD_LOCATIONS.at(0)?.name, title: "Datacenter" },
+    { description: instance?.pveNode, title: "Node" },
+    {
+      description: `🇨🇦 ${CLOUD_LOCATIONS.at(0)?.location.split(",")[1]}`,
+      title: "Country",
+    },
+    { description: CLOUD_LOCATIONS.at(0)?.region, title: "Network Zone" },
+  ]
+
   return (
-    <div>
-      <h1>InstanceLocation</h1>
-    </div>
+    <Card className="@container gap-4">
+      <CardHeader className="px-8">
+        <CardTitle className="flex items-center gap-2">
+          <IconMapPin className="size-5 stroke-primary" />
+          <p className="text-lg uppercase">Location</p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex h-full @md:flex-row flex-col @md:items-center justify-between @md:gap-0 gap-4 px-4">
+        <div className="grid flex-2 @md:grid-cols-2 grid-cols-1 gap-4">
+          {LOCATION_DETAILS.map((detail) => (
+            <Item key={detail.title}>
+              <ItemContent>
+                <ItemTitle className="font-normal text-muted-foreground text-sm">
+                  {detail.title}
+                </ItemTitle>
+                {detail.description && (
+                  <Hint label={detail.description} side="inline-start">
+                    <ItemDescription className="line-clamp-1 font-medium text-foreground">
+                      {detail.description}
+                    </ItemDescription>
+                  </Hint>
+                )}
+              </ItemContent>
+            </Item>
+          ))}
+        </div>
+        <div className="@md:inline-flex hidden h-full flex-1 items-center">
+          <CountryMap />
+        </div>
+      </CardContent>
+    </Card>
   )
 }
--- src/modules/dashboard/ui/instance-details/options.tsx
+++ src/modules/dashboard/ui/instance-details/options.tsx
@@ -1,7 +1,70 @@
+"use client"
+
+import { IconSettings, IconWorld } from "@tabler/icons-react"
+import { DatabaseBackupIcon, GroupIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Item, ItemActions, ItemContent, ItemMedia } from "@/components/ui/item"
+
 export function InstanceOptions({ instanceId }: { instanceId: string }) {
   return (
-    <div>
-      <h1>InstanceOptions</h1>
-    </div>
+    <Card className="gap-4">
+      <CardHeader className="px-8">
+        <CardTitle className="flex items-center gap-2">
+          <IconSettings className="size-5 stroke-primary" />
+          <p className="text-lg uppercase">Options</p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="px-4">
+        <div className="flex flex-row flex-wrap">
+          <Item className="w-auto shrink-0 items-start gap-2">
+            <ItemMedia className="mt-1" variant="icon">
+              <DatabaseBackupIcon className="size-6 stroke-primary" />
+            </ItemMedia>
+            <div className="flex flex-col gap-2">
+              <ItemActions>
+                <Button onClick={() => alert("Coming soon!")}>Enable</Button>
+              </ItemActions>
+              <ItemContent>
+                <p className="ml-1 uppercase">Backups</p>
+              </ItemContent>
+            </div>
+          </Item>
+
+          <Item className="w-auto shrink-0 items-start gap-2">
+            <ItemMedia className="mt-1" variant="icon">
+              <GroupIcon className="size-6 stroke-primary" />
+            </ItemMedia>
+            <div className="flex flex-col gap-2">
+              <ItemActions>
+                <Button disabled onClick={() => alert("Coming soon!")}>
+                  Select group
+                </Button>
+              </ItemActions>
+              <ItemContent>
+                <p className="ml-1 uppercase">Placement Group</p>
+              </ItemContent>
+            </div>
+          </Item>
+
+          <Item className="w-auto shrink-0 items-start gap-2">
+            <ItemMedia className="mt-1" variant="icon">
+              <IconWorld className="size-6 stroke-primary" />
+            </ItemMedia>
+            <div className="flex flex-col gap-2">
+              <ItemActions>
+                <Button disabled onClick={() => alert("Coming soon!")}>
+                  Disable
+                </Button>
+              </ItemActions>
+              <ItemContent>
+                <p className="ml-1 uppercase">Public Network</p>
+              </ItemContent>
+            </div>
+          </Item>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
