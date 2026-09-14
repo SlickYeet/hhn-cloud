509271bb23971db570915a5694f4aff323ec439a
SlickYeet <lasse@lx2.dev>
2026-09-14 00:20:58 -0700

    redirect home page to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import { IconArrowRight } from "@tabler/icons-react"
-import Link from "next/link"
-
-import { Button } from "@/components/ui/button"
+import { redirect } from "next/navigation"
 
 export default async function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
-      <h1>Page</h1>
-      <Button
-        className="mt-4"
-        nativeButton={false}
-        render={<Link href="/dashboard" />}
-        size="lg"
-      >
-        Get Started <IconArrowRight />
-      </Button>
-    </main>
-  )
+  return redirect("/dashboard")
 }
