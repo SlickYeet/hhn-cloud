e11b43b36340ba42c8203a556cf95b3865b0079e
SlickYeet <lasse@lx2.dev>
2026-09-20 18:53:54 -0700

    add constrains for comment and name, and default type to ed25519

--- src/schemas/ssh-key.ts
+++ src/schemas/ssh-key.ts
@@ -1,17 +1,21 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod"
 import * as z from "zod"
 
-import { sshKeyTable } from "@/server/db/schema"
+import { sshKeyTable, sshKeyTypeEnum } from "@/server/db/schema"
 
 const sshKeyConstraints = {
+  comment: z.string().max(255).nullish(),
   createdAt: z.coerce.date().optional(),
+  name: z.string().min(3).max(255),
   updatedAt: z.coerce.date().optional(),
 }
 
 export const insertSSHKeySchema = createInsertSchema(
   sshKeyTable,
   sshKeyConstraints,
-)
+).extend({
+  type: z.enum(sshKeyTypeEnum.enumValues).default("ed25519"),
+})
 export const selectSSHKeySchema = createSelectSchema(
   sshKeyTable,
   sshKeyConstraints,
